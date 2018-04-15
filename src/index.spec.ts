74e6ae60afb41ea027ab4559b0d080d974ea91a0
Marvin Hagemeister <marvin@marvinhagemeister.de>
2018-04-15 08:15:27 +0200

    Import implementation

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,41 @@
-describe("foo", () => {
-  it("should return foo", () => {
-    const foo = () => "foo";
-    expect(foo()).toEqual("foo");
+import * as t from "assert";
+import { CancelController, wrap, throwIfAborted } from ".";
+
+const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+describe("throwIfAborted", () => {
+  it("should throw", () => {
+    const c = new CancelController();
+    try {
+      c.abort();
+      throwIfAborted(c.signal);
+    } catch (err) {
+      if (err !== "Aborted") throw err;
+    }
+  });
+});
+
+describe("wrap", () => {
+  it("should forward result", async () => {
+    const c = new CancelController();
+    const p = delay(1).then(() => 1);
+    const res = await wrap(p, c.signal);
+    t.equal(res, 1);
+  });
+
+  it("should not continue with promise", async () => {
+    const p = Promise.resolve();
+    const c = new CancelController();
+
+    let thrown = false;
+    try {
+      c.abort();
+      await wrap(p, c.signal).then(() => t.fail("should not happen"));
+    } catch (err) {
+      thrown = true;
+      t.equal(err, "Aborted");
+    } finally {
+      t.equal(thrown, true);
+    }
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,37 @@
-// tslint:disable-next-line
-console.log("hello world");
+export const AbortedErrorMsg = "Aborted";
+
+export class CancelSignal {
+  aborted = false;
+
+  abort() {
+    this.aborted = true;
+  }
+}
+
+export class CancelController {
+  signal = new CancelSignal();
+
+  abort() {
+    this.signal.abort();
+  }
+}
+
+/**
+ * Throwing error objects is slower because of stack trace creation.
+ */
+export function throwIfAborted(signal: CancelSignal | AbortSignal) {
+  if (signal.aborted) throw AbortedErrorMsg;
+}
+
+/**
+ * Rejects a promise with the string `Aborted` if the signal is aborted.
+ */
+export function wrap<T>(
+  promise: Promise<T>,
+  signal: CancelSignal | AbortSignal,
+): Promise<T> {
+  return new Promise((resolve, reject) => {
+    if (signal.aborted) return reject(AbortedErrorMsg);
+    resolve(promise);
+  });
+}
